d9fa1bd9e08e9c77c7ba94ab684149da92b79bf3
01375789 <pinfanyao@sf-express.com>
2018-04-25 19:19:13 +0800

    add example plugin project in repo

---
+++ example4plugina/app/src/main/java/com/example/fantasy1722/example4plugina/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fantasy1722.example4plugina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
